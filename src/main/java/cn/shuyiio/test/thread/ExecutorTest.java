8b901ee0736cdfe290c917fd414194cb0109f6f4
Stephenzho <stephen_zho@126.com>
2019-05-22 19:18:39 +0800

    thread test

---
+++ src/main/java/cn/shuyiio/test/thread/ExecutorTest.java
@@ -0,0 +1,30 @@
+package cn.shuyiio.test.thread;
+
+import org.junit.Test;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+public class ExecutorTest {
+
+
+
+    @Test
+    public void exeTest(){
+
+        Executors.newCachedThreadPool();
+
+        Executors.newFixedThreadPool(12);
+
+        ScheduledExecutorService ses = Executors.newScheduledThreadPool(12);
+
+
+
+        Executors.newSingleThreadExecutor();
+
+        Executors.newWorkStealingPool();
+
+
+    }
+
+}
